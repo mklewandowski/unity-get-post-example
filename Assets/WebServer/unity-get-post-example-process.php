619445162b1749bcdff1becf1bb6224c5cdcb553
mklewandowski <matt.kain.lewandowski@gmail.com>
2022-06-08 09:40:43 -0700

    Add simple HTML form for testing, add logging and output to PHP script

--- Assets/WebServer/unity-get-post-example-process.php
+++ Assets/WebServer/unity-get-post-example-process.php
@@ -3,19 +3,21 @@
 <body>
 
 <?php
+	echo "Answer successfully submitted!";
+	echo "<br>";
+	echo "Your answer index is:";
+	echo "<br>";
 	echo $_POST["answer-index"];
 	echo "<br>";
 	$fileContents = file_get_contents("unity-get-post-example-process.txt");
+	echo "Previous contents of CSV text file:";
+	echo "<br>";
 	echo $fileContents;
 	echo "<br>";
 	
 	$textArray = explode(",", $fileContents);
 	$txt = "";
 	$arrLength = count($textArray);
-	echo $textArray;
-	echo "<br>";
-	echo $arrLength;
-	echo "<br>";
 	for($x = 0; $x < $arrLength; $x++) {
 		if ($x == $_POST["answer-index"]) {
 			$newVal = $textArray[$x] + 1;
@@ -27,10 +29,15 @@
 			$txt = $txt . ",";
 		}
 	}
+	echo "New contents of CSV text file:";
+	echo "<br>";
+	echo $txt;
+	echo "<br>";
 	$myfile = fopen("unity-get-post-example-process.txt", "w") or die("Unable to open file!");
 	fwrite($myfile, $txt);
 	fclose($myfile);
 ?>
+The <a href="unity-get-post-example-process.txt">CSV text file</a> with the answers can be seen <a href="unity-get-post-example-process.txt">here</a> (you might need to refresh your browser to see the latest values).
 
 </body>
 </html>
